b8a7bf83146c0dda1e01c3197d6201a1613f0d1e
shahharsh093 <shah.harsh093@gmail.com>
2016-06-30 00:20:13 +0530

    10,2:15,registered Signup model in admin

--- newsletter/models.py
+++ newsletter/models.py
@@ -3,3 +3,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class SignUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(max_length=100,blank=True, null=True)
+    timetamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self): #Python 3.3 is __str__
+        return self.email
